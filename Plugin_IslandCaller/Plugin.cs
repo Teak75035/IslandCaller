5fdb490a67dd00efce54647e40aa3fc5e03d4d5e
LiPolymer <fish@lipoly.ink>
2025-03-22 15:37:25 +0800

    feat: 增加悬浮窗口

--- Plugin_IslandCaller/Plugin.cs
+++ Plugin_IslandCaller/Plugin.cs
@@ -1,10 +1,14 @@
-﻿using ClassIsland.Core.Abstractions;
+﻿using ClassIsland.Core;
+using ClassIsland.Core.Abstractions;
 using ClassIsland.Core.Attributes;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using IslandCaller.Services.NotificationProviders;
 using ClassIsland.Core.Extensions.Registry;
 using IslandCaller.Views.SettingsPages;
+using Plugin_IslandCaller.Views.Windows;
+using System.IO;
+
 namespace IslandCaller;
 
 [PluginEntrance]
@@ -14,5 +18,13 @@ public class Plugin : PluginBase
     {
         services.AddHostedService<IslandCallerNotificationProvider>();
         services.AddSettingsPage<IslandCallerSettingsPage>();
+        Hover.ConfigFolder = PluginConfigFolder;
+        AppBase.Current.AppStarted += (_,_) => {
+            if (File.Exists(Path.Combine(PluginConfigFolder,"enableHover.flag")))
+            {
+                Hover.Instance ??= new Hover();
+                Hover.Instance.Show();
+            }
+        };
     }
 }
---
+++ Plugin_IslandCaller/Views/Windows/Hover.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ClassIsland.Core;
+using ClassIsland.Core.Commands;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace Plugin_IslandCaller.Views.Windows;
+
+public partial class Hover : Window
+{
+    public static Hover? Instance;
+    public static string? ConfigFolder;
+
+    public Hover()
+    {
+        InitializeComponent();
+    }
+
+    void Hover_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        UriNavigationCommands.UriNavigationCommand.Execute("classisland://plugins/IslandCaller/Run");
+    }
+}
